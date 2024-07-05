a9e4ba106244ea7153c98f744b6f43a679738634
Ethernal24 <gilbertgiovanni14@gmail.com>
2024-07-05 19:24:29 +0700

    regis confirm password ui

--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -29,6 +29,15 @@
                     </span>
                 </div>
             </div>
+            <div class="input-group d-flex mx-auto w-50">
+                <h5 class="title-email">Confirm Password</h5>
+                <div class="input-group d-flex mx-auto">
+                    <input class="form-control" type="password" name="password_confirmation" placeholder="Masukkan Password" aria-describedby="eyes-icon" id="password1" required>
+                    <span id="eyes-icon" class="input-group-text p-3">
+                        <i id="togglePassword1" class="fa fa-eye"></i>
+                    </span>
+                </div>
+            </div>
             <button type="submit" class="btn btn-primary to-login w-50 mx-auto py-2 border-0 border-secondary position-relative d-flex rounded-pill text-white text-center justify-content-center">Register</button>
             <div class="d-flex mx-auto w-50 justify-content-center">
                 <span>Already have account?</span>
